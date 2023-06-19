6b91b2911350aff7707307b5268264661ae64405
Ryvqi <rifqikhabibi03@gmail.com>
2023-06-19 15:39:52 +0700

    memanggil

--- ExeptionStandard/ExeptionStandard.cpp
+++ ExeptionStandard/ExeptionStandard.cpp
@@ -6,5 +6,12 @@ using namespace std;
 //untuk objek yang akan kita guanakan
 
 int main() {
-
+	cout << "Awal Program" << endl; //penanda 1:..
+	try
+	{
+		array<int, 3> data = { 1,2,3 };
+		//pesan array integer 3 elemen
+		cout << data.at(4) << endl;
+		//memanggil array elemen ke 5
+	}
 }
