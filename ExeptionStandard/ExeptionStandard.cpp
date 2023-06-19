ee2e01feac5cc677170ba79093ee3c7e8c31d537
Ryvqi <rifqikhabibi03@gmail.com>
2023-06-19 15:37:32 +0700

    include array

--- ExeptionStandard/ExeptionStandard.cpp
+++ ExeptionStandard/ExeptionStandard.cpp
@@ -1,2 +1,10 @@
 #include <iostream>
+#include <exception>
 using namespace std;
+//untuk objek exeption yang akan digunakan 
+#include <array>
+//untuk objek yang akan kita guanakan
+
+int main() {
+
+}
