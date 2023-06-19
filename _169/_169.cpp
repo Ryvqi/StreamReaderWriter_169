46d65222e7b34f449238eb2f37e8de7f77aab7f9
Ryvqi <rifqikhabibi03@gmail.com>
2023-06-19 14:46:20 +0700

    outfile

--- _169/_169.cpp
+++ _169/_169.cpp
@@ -6,5 +6,8 @@ using namespace std;
 int main() {
 	string baris;
 
-	//
+	//membuka file dalam bentuk menulis
+	ofstream outfile;
+	//menunjuk ke sebuah nama file
+	outfile.open("contohfile.txt");
 }
