183ad9e52e3798cc4c9baa48ebe60f6b81606b72
Ryvqi <rifqikhabibi03@gmail.com>
2023-06-19 15:12:09 +0700

    outfile

--- iofilewithpath/iofilewithpath.cpp
+++ iofilewithpath/iofilewithpath.cpp
@@ -16,4 +16,15 @@ int main() {
 	outfile.open(NamaFile + ".txt", ios::out);
 
 	cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+	//unlimited loop untuk menulis
+	while (true) {
+		cout << "- ";
+		//mendapatkan setiap karakter dalam satu baris
+		getline(cin, baris);
+		//loop akan berhenti jika anda memasukkan huruf q
+		if (baris == "q") break;
+		//menulis dan memasukan nilai dari 'baris' ke dalam file
+		outfile << baris << endl;
+	}
 }
