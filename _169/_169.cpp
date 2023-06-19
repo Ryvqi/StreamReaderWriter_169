4082a4e20badfe8946f54e8d8089e879b38e8ee4
Ryvqi <rifqikhabibi03@gmail.com>
2023-06-19 14:59:51 +0700

    if

--- _169/_169.cpp
+++ _169/_169.cpp
@@ -30,4 +30,11 @@ int main() {
 	ifstream infile;
 	//menunjuk ke sebuah file
 	infile.open("contohfile.txt");
+
+	cout << endl << ">= Membuka dan Membaca file" << endl;
+	//jika file ada maka
+	if (infile.is_open())
+	{
+
+	}
 }
